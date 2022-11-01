67a464f718fc1109be1b8e9c669544460ff05ffb
Shinamin_97 <qnrlgkals@naver.com>
2022-11-01 16:55:48 +0900

    카카오채팅

---
+++ 3_JAVASCRIPT/js/01_JS개요.js
@@ -0,0 +1,15 @@
+// js파일은 <script> 태그 내부라고 생각하면 된다.
+
+function btnClick3() {
+    alert("external 버튼이 클릭되었습니다.");
+}
+
+function changeColor1() {
+document.getElementById("box").style.backgroundColor = "red";
+}
+
+function changeColor2() {
+document.getElementById("box").style.backgroundColor = "yellow";
+}
+
+
